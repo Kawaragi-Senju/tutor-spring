45ad775388518ed9ceb1ef88cb9252c1de7ac33f
danny <dannyboykov@bk.ru>
2024-08-15 15:08:26 +0300

    commit.

---
+++ src/test/java/ru/daniilboikov/tutorspring/TutorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daniilboikov.tutorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
